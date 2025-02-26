04bd90cc32a22f926c61fa64462b191b84048641
ayush_0001 <ayushk0004@gmail.com>
2025-02-26 11:25:47 +0530

    Swap two num without third variable using xor

---
+++ Array/Swap.java
@@ -0,0 +1,33 @@
+// public class Swap 
+// {
+//     static void swapNum(int[] arr) 
+//     {
+//         arr[0] = arr[0] + arr[1];
+//         arr[1] = arr[0] - arr[1];
+//         arr[0] = arr[0] - arr[1];
+//     }
+//     public static void main(String[] args) 
+//     {
+//         int[] nums = {5, 10};
+//         System.out.println("Before swapping: a = " + nums[0] + ", b = " + nums[1]);
+//         swapNum(nums);
+//         System.out.println("After swapping: a = " + nums[0] + ", b = " + nums[1]);
+//     }
+// }
+
+// public class Swap 
+// {
+    // static void swapNum(int[] arr) 
+    // {
+    //     arr[0] = arr[0] ^ arr[1];
+    //     arr[1] = arr[0] ^ arr[1];
+    //     arr[0] = arr[0] ^ arr[1];
+    // }
+//     public static void main(String[] args) 
+//     {
+//         int[] nums = {5, 10};
+//         System.out.println("Before swapping: a = " + nums[0] + ", b = " + nums[1]);
+//         swapNum(nums);
+//         System.out.println("After swapping: a = " + nums[0] + ", b = " + nums[1]);
+//     }
+// }
