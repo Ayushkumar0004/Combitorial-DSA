384560524464d73bd57bc1f754d9e0681f6b5071
ayush_0001 <ayushk0004@gmail.com>
2025-02-26 11:32:27 +0530

    Swap

--- Array/Swap.java
+++ Array/Swap.java
@@ -17,17 +17,16 @@
 
 // public class Swap 
 // {
-    // static void swapNum(int[] arr) 
-    // {
-    //     arr[0] = arr[0] ^ arr[1];
-    //     arr[1] = arr[0] ^ arr[1];
-    //     arr[0] = arr[0] ^ arr[1];
-    // }
 //     public static void main(String[] args) 
 //     {
-//         int[] nums = {5, 10};
-//         System.out.println("Before swapping: a = " + nums[0] + ", b = " + nums[1]);
-//         swapNum(nums);
-//         System.out.println("After swapping: a = " + nums[0] + ", b = " + nums[1]);
+//         int a = 5; 
+//         // 0101
+//         int b = 10;
+//         // 1010
+//         System.out.println("Before swapping: a = " + a + ", b = " + b);
+//         a = a ^ b;
+//         b = a ^ b;
+//         a = a ^ b;
+//         System.out.println("After swapping: a = " + a + ", b = " + b);
 //     }
 // }
