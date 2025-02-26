139d27a04ab9e8701f76c51feb9efa8d87556440
ayush_0001 <ayushk0004@gmail.com>
2025-02-26 10:46:45 +0530

    find max among three element

--- Array/max_ele.java
+++ Array/max_ele.java
@@ -1,30 +1,30 @@
-// package Array;
-import java.util.Scanner;
-public class max_ele 
-{
-    static int find_max(int a, int b, int c)
-    {
-        int max = a;
-        if(b  > max)
-        {
-            max = b;
-        }
-        if(c > max)
-        {
-            max = c;
-        }
-        return max;
-    }
-    public static void main(String[] args) 
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the num1: ");
-        int num1 = sc.nextInt();
-        System.out.println("Enter the num2: ");
-        int num2 = sc.nextInt();
-        System.out.println("Enter the num3: ");
-        int num3 = sc.nextInt();
-        int max = find_max(num1, num2, num3);
-        System.out.println("Max element is: " + max);
-    }    
-}
+// // package Array;
+// import java.util.Scanner;
+// public class max_ele 
+// {
+//     static int find_max(int a, int b, int c)
+//     {
+//         int max = a;
+//         if(b  > max)
+//         {
+//             max = b;
+//         }
+//         if(c > max)
+//         {
+//             max = c;
+//         }
+//         return max;
+//     }
+//     public static void main(String[] args) 
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the num1: ");
+//         int num1 = sc.nextInt();
+//         System.out.println("Enter the num2: ");
+//         int num2 = sc.nextInt();
+//         System.out.println("Enter the num3: ");
+//         int num3 = sc.nextInt();
+//         int max = find_max(num1, num2, num3);
+//         System.out.println("Max element is: " + max);
+//     }    
+// }
