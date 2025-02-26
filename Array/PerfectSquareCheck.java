5179d057b5f3bb2b6ef2ace159655fd6cc5d2f5b
ayush_0001 <ayushk0004@gmail.com>
2025-02-26 11:50:30 +0530

    PerfectSquareCheck  without inbuilt func

---
+++ Array/PerfectSquareCheck.java
@@ -0,0 +1,23 @@
+// import java.util.Scanner;
+// public class PerfectSquareCheck 
+// {
+//     public static boolean isPerfectSquare(int num) 
+//     {
+//         if (num < 0) return false;
+//         for (int i = 0; i * i <= num; i++) 
+//         {
+//             if (i * i == num) 
+//             {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+//     public static void main(String[] args) 
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number: ");
+//         int num = sc.nextInt();
+//         System.out.println(isPerfectSquare(num));
+//     }
+// }
