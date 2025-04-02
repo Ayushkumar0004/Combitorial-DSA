54e05974b68fe7d0005b09156b2216fe07a42cdd
ayush_0001 <ayushk0004@gmail.com>
2025-04-02 10:48:51 +0530

    Prime number Product

--- Practice/Prime_Product.java
+++ Practice/Prime_Product.java
@@ -1,3 +1,41 @@
+// import java.util.Scanner;
+
+// public class Prime_Product 
+// {
+//     public static void main(String[] args) 
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the num: ");
+//         int n = sc.nextInt();
+//         int arr[] = new int[n];  
+//         int count = 0;
+//         for (int i = 2; i <= n; i++) 
+//         {
+//             boolean isPrime = true;
+//             for (int j = 2; j < i; j++) 
+//             { 
+//                 if (i % j == 0) 
+//                 {
+//                     isPrime = false;
+//                     break;
+//                 }
+//             }
+//             if (isPrime) 
+//             {
+//                 arr[count] = i;
+//                 count++;
+//             }
+//         }
+//         long product = 1;
+//         for (int i = 0; i < count; i++) 
+//         {
+//             product *= arr[i];
+//         }
+//         System.out.println("Product of prime numbers is: " + product);
+//         sc.close();
+//     }
+// }
+
 import java.util.Scanner;
 
 public class Prime_Product 
@@ -9,10 +47,12 @@ public class Prime_Product
         int n = sc.nextInt();
         int arr[] = new int[n];  
         int count = 0;
+        System.out.print("Prime numbers: ");
+
         for (int i = 2; i <= n; i++) 
         {
             boolean isPrime = true;
-            for (int j = 2; j < i; j++) 
+            for (int j = 2; j * j <= i; j++)  // Optimized prime check
             { 
                 if (i % j == 0) 
                 {
@@ -24,13 +64,17 @@ public class Prime_Product
             {
                 arr[count] = i;
                 count++;
+                System.out.print(i + " "); // Display each prime number
             }
         }
+
+        System.out.println(); // Move to next line
         long product = 1;
         for (int i = 0; i < count; i++) 
         {
             product *= arr[i];
         }
+        
         System.out.println("Product of prime numbers is: " + product);
         sc.close();
     }
