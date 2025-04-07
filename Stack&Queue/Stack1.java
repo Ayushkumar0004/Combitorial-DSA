45bbd1170b68f7535f0e9909901e308bd727617a
ayush_0001 <ayushk0004@gmail.com>
2025-04-07 10:36:16 +0530

    reverse the string using stack

---
+++ Stack&Queue/Stack1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Stack1
+{
+    public static String reverseString(String input) 
+    {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) 
+        {
+            stack.push(c);
+        }
+        StringBuilder reversed = new StringBuilder();
+        while (!stack.isEmpty()) 
+        {
+            reversed.append(stack.pop());
+        }
+        return reversed.toString();
+    }
+
+    public static void main(String[] args) 
+    {
+        String ip = "Hello World";
+        String reversed = reverseString(ip);
+        System.out.println("Original: " + ip);
+        System.out.println("Reversed: " + reversed);
+    }
+}
