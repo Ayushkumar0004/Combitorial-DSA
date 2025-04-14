e0545014c24bbe1e700c0257539da1ca58f9b1ea
ayush_0001 <ayushk0004@gmail.com>
2025-04-14 10:50:28 +0530

    Intro of Recursion

---
+++ Recursion/Task1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Task1
+{
+    public static int rec(int n)
+    {
+        if(n == 5)
+        {
+            return 5;
+        }
+        System.out.println(n);
+        return rec(n + 1);
+    }
+    public static void main(String[] args) 
+    {
+        rec(1);
+    }
+}
